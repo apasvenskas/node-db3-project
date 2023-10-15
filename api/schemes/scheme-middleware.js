da44218d34cd7cbd65bfe186dea0a7ce2299fea8
Audrius Pasvenskas <apasvenskas@outlook.com>
2023-10-15 01:01:49 -0700

    all test passing

--- api/schemes/scheme-middleware.js
+++ api/schemes/scheme-middleware.js
@@ -9,13 +9,13 @@ const db = require('../../data/db-config')
 */
 const checkSchemeId = async (req, res, next) => {
   try {
-    const existing = await db('shemes')
-    .where('schema_id', req.params.scheme_id)
+    const existing = await db('schemes')
+    .where('scheme_id', req.params.scheme_id)
     .first()
     if(!existing){
       next({
         status: 404,
-        message: `shceme with scheme_id ${req.params.scheme_id} not found`,
+        message: `scheme with scheme_id ${req.params.scheme_id} not found`,
       })
     } else {
       next()
@@ -34,11 +34,11 @@ const checkSchemeId = async (req, res, next) => {
   }
 */
 const validateScheme = (req, res, next) => {
-  const { shceme_name } = req.body
+  const { scheme_name } = req.body
   if (
-    shceme_name === undefined ||
-    typeof shceme_name !== 'string' ||
-    !shceme_name.trim()
+    scheme_name === undefined ||
+    typeof scheme_name !== 'string' ||
+    !scheme_name.trim()
   ) {
     next({ status: 400, message: 'invalid scheme_name'})
   } else {
